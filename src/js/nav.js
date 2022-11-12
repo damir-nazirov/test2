870ad861b4b4ca38345837015f511c2d601a5711
Damir Nazirov <damirin500@gmail.com>
2022-11-12 11:29:01 +0300

    доработка вёрстки

--- src/js/nav.js
+++ src/js/nav.js
@@ -32,32 +32,56 @@ const burger = document.querySelector('.button_burger');
 const closeNav = document.querySelector('.button_close');
 const mainNav = document.querySelector('.main-nav');
 const mainContent = document.querySelector('.main-content');
+
+console.log(mainNav);
 // const header = document.querySelector('.header');
 
-export const addClass = (element, className) => {
-  element.classList.add(className)
+// export const addClass = (element, className) => {
+//   element.classList.add(className)
+// }
+
+// export const removeClass = (element, className) => {
+//   element.classList.remove(className)
+// }
+
+
+// const showNav = () => {
+//       addClass(mainNav, 'display_flex');
+//       addClass(mainContent, 'main-content_opasity_01')
+//       // addClass(header, 'display_none')
+//     }
+
+// const hideNav = () => {
+//   removeClass(mainNav, 'display_flex');
+//   removeClass(mainContent, 'main-content_opasity_01');
+// }
+
+
+export const toggleClass = (element, className ) => {
+        if (element.classList.contains(className)) {
+          element.classList.remove(className)
+        }
+        else {element.classList.add(className)}
 }
 
-export const removeClass = (element, className) => {
-  element.classList.remove(className)
+const toggleShowNav = () => {
+  toggleClass(mainNav, 'hide')
+  toggleClass(mainContent, 'main-content_opasity_01')
+  // addClass(header, 'display_none')
 }
 
-
-const showNav = () => {
-      addClass(mainNav, 'display_flex');
-      addClass(mainContent, 'main-content_opasity_01')
-      // addClass(header, 'display_none')
-    }
-
-const hideNav = () => {
-  removeClass(mainNav, 'display_flex');
-  removeClass(mainContent, 'main-content_opasity_01');
-}
-
-// const toggleClass
+// const hideNav = () => {
+// removeClass(mainNav, 'display_flex');
+// removeClass(mainContent, 'main-content_opasity_01');
+// }
 
 
 
 
-burger.addEventListener('click', showNav);
-closeNav.addEventListener('click', hideNav);
+burger.addEventListener('click', toggleShowNav);
+closeNav.addEventListener('click', toggleShowNav);
+
+
+
+// burger.addEventListener('click', showNav);
+// closeNav.addEventListener('click', hideNav);
--- src/js/services.js
+++ src/js/services.js
@@ -1,52 +1,40 @@
-import { addClass } from "./nav";
-import { removeClass } from "./nav";
+// import { addClass } from "./nav";
+// import { removeClass } from "./nav";
+import { toggleClass } from "./nav";
 
-const servicesDescriptionTextHidden = document.querySelector('.services__description-text_hidden');
+const servicesDescriptionTextHidden = document.querySelector('.services__description-text_hide');
 
 const readMoreDescr = document.querySelector('.services__description-text .show-hide-button_show');
 const hideMoreDescr = document.querySelector('.services__description-text .show-hide-button_hide');
 
 
 
-// console.log(hide_more_brends);
-// const read_more_descr_button = document.querySelector('.services__description-button_show')
 
-const showMoreDescrText = () => {
-    addClass(servicesDescriptionTextHidden, 'display_flex');
-    addClass(readMoreDescr, 'display_none');
-    addClass(hideMoreDescr, 'display_flex');
+const showHideMoreDescrText = () => {
+    toggleClass(readMoreDescr, 'hide');
+    toggleClass(hideMoreDescr, 'hide')
+    toggleClass(servicesDescriptionTextHidden, 'hide')
   }
 
-  const hideMoreDescrText = () => {
-    removeClass(servicesDescriptionTextHidden, 'display_flex');
-    removeClass(readMoreDescr, 'display_none');
-    removeClass(hideMoreDescr, 'display_flex');
-  }
 
-  readMoreDescr.addEventListener('click', showMoreDescrText)
-  hideMoreDescr.addEventListener('click', hideMoreDescrText)
+  readMoreDescr.addEventListener('click', showHideMoreDescrText)
+  hideMoreDescr.addEventListener('click', showHideMoreDescrText)
 
 ///////////////////////////////////////////////////////////////////////
 
 
-const servicesBrendsHidden = document.querySelector('.services__brands-logo-wrapper_hidden');
+const servicesBrendsHidden = document.querySelector('.services__brands-logo-wrapper .hide');
 
 const showMoreBrends = document.querySelector('.services__brands-logo .show-hide-button_show');
 const hideMoreBrends = document.querySelector('.services__brands-logo .show-hide-button_hide');
 
-// console.log(show_more_brends);
 
-const onShowMoreBrends = () => {
-  addClass(servicesBrendsHidden, 'display_flex');
-  addClass(showMoreBrends, 'display_none');
-  addClass(hideMoreBrends, 'display_flex');
+const showHideMoreBrends = () => {
+  toggleClass(servicesBrendsHidden, 'hide');
+  toggleClass(showMoreBrends, 'hide');
+  toggleClass(hideMoreBrends, 'hide');
+
 }
 
-const onHideMoreBrends = () => {
-  removeClass(servicesBrendsHidden, 'display_flex');
-  removeClass(showMoreBrends, 'display_none');
-  removeClass(hideMoreBrends, 'display_flex');
-}
-
-showMoreBrends.addEventListener('click', onShowMoreBrends)
-hideMoreBrends.addEventListener('click', onHideMoreBrends)
+showMoreBrends.addEventListener('click', showHideMoreBrends)
+hideMoreBrends.addEventListener('click', showHideMoreBrends)
